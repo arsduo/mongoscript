2fe0f3b9226cdd42fd817a34f098c082cd3ffd67
Alex Koppel <alex+git@alexkoppel.com>
2012-02-09 22:05:13 +0100

    Set up spec_runner and Guardfile

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+require 'mongoid'
+require 'mongoscript'
+
+RSpec.configure do |config|
+
+  config.mock_with :mocha
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+end
