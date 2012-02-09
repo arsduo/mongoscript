cfdf44a1117e79d62d9548931c67db2a9ce97f5c
Alex Koppel <alex+git@alexkoppel.com>
2012-02-09 22:05:50 +0100

    Added MongoScript.orm_adapter + specs

--- lib/mongoscript.rb
+++ lib/mongoscript.rb
@@ -1,5 +1,26 @@
+require "mongoscript/orm/mongoid"
 require "mongoscript/version"
 
-module Mongoscript
-  # Your code goes here...
+module MongoScript
+  class NoORMError < StandardError; end
+
+  # Returns the MongoScript adapter module for
+  # whichever Mongo ORM is loaded (Mongoid, MongoMapper).
+  #
+  # @note: currently only Mongoid is supported.
+  #
+  # @raises NoORMError if no ORM module can be detected.
+  #
+  # @returns MongoScript::ORM::Mongoid if Mongoid is detected
+  def self.orm_adapter
+    if const_defined? "Mongoid"
+      MongoScript::ORM::Mongoid
+    else
+      raise NoORMError, "Unable to locate Mongoid!"
+    end
+  end
 end
+
+# these are defined afterward since they depend on the ORM adapter
+# TODO: fix this
+require "mongoscript/execution"
---
+++ spec/cases/mongoscript_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe MongoScript do
+  describe ".orm_adapter" do
+    it "returns the Mongoid adapter if Mongoid is defined" do
+      Object.stubs(:const_defined?).with("Mongoid").returns(true)
+      MongoScript.orm_adapter.should == MongoScript::ORM::Mongoid
+    end
+
+    it "raises a NoORMError if no Mongo ORM is available" do
+      MongoScript.stubs(:const_defined?).with("Mongoid").returns(false)
+      expect { MongoScript.orm_adapter }.to raise_exception(MongoScript::NoORMError)
+    end
+  end
+end
