aec71fb073895bc03407101e4378f86a3c09b8ce
Alex Koppel <alex+git@alexkoppel.com>
2012-02-11 12:37:48 +0100

    Spec for resolve_arguments and resolve_complex_criteria

--- spec/cases/mongoid_adapter_spec.rb
+++ spec/cases/mongoid_adapter_spec.rb
@@ -31,4 +31,27 @@ describe MongoScript::ORM::MongoidAdapter do
       @adapter.rehydrate("foo", "bar").should == result
     end
   end
+
+  describe "#resolve_arguments" do
+    it "resolves any hashes in the arguments" do
+      args = [{}, 2, 3, [], {:c.in => 2}]
+      args.each {|a| @adapter.expects(:resolve_complex_criteria).with(a) if a.is_a?(Hash)}
+      @adapter.resolve_arguments(args)
+    end
+
+    it "returns the mapped results" do
+      args = [{}, 2, 3, [], {:c.in => 2}]
+      stubby = stub("result")
+      @adapter.stubs(:resolve_complex_criteria).returns(stubby)
+      @adapter.resolve_arguments(args).should == args.map {|a| a.is_a?(Hash) ? stubby : a}
+    end
+  end
+
+  describe "#resolve_complex_criteria" do
+    it "recursively replaces any hash values with their own resolve_complex_criteria results" do
+      hash = {:a.in => [1, 2], :c => {:e.exists => false, :f => {:y.ne => 2}}, :f => 3}
+      @adapter.resolve_complex_criteria(hash).should ==
+        {:a=>{"$in"=>[1, 2]}, :c=>{:e=>{"$exists"=>false}, :f=>{:y=>{"$ne"=>2}}}, :f=>3}
+    end
+  end
 end
