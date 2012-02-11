05187692350f0473e9adb8cfe4033791fb44fc8a
Alex Koppel <alex+git@alexkoppel.com>
2012-02-11 13:17:40 +0100

    Add placeholder for Mongo extension method

---
+++ lib/mongoscript/orm/mongoid_document_methods.rb
@@ -0,0 +1,11 @@
+# TBD: do we need this?
+module MongoidDocumentMethods
+  module ClassMethods
+    def find_by_javascript(script_name, *args)
+      args = args.unshift(script_name)
+      # get a bunch of results via a Mongoid Javascript call,
+      # then turn each hash result into a Mongoid document
+      MongoScript.execute_readonly_routine(*args).map { |hash| rehydrate(self, hash) }
+    end
+  end
+end
