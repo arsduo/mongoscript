389cdb294dc38447b6a2d5b9d498bf3467059e01
Alex Koppel <alex+git@alexkoppel.com>
2012-02-11 14:28:56 +0100

    Finished specs for Execution

--- lib/mongoscript/execution.rb
+++ lib/mongoscript/execution.rb
@@ -12,62 +12,72 @@ module MongoScript
   # see http://www.mongodb.org/display/DOCS/Server-side+Code+Execution#Server-sideCodeExecution-Storingfunctionsserverside
 
   module Execution
+
     LOADED_SCRIPTS = {}
 
     class ScriptNotFound < Errno::ENOENT; end
     class NoScriptDirectory < ArgumentError; end
+    class ExecutionFailure < RuntimeError; end
 
-    class << self
-      attr_accessor :script_dir
+    def self.included(base)
+      base.class_eval do
+        class << self
+          attr_accessor :script_dir
+        end
+
+        extend MongoScript::Execution::ClassMethods
+      end
     end
 
-    # code from stored files
-    def self.code_for(script_name, dir = self.script_dir)
-      script_name = script_name.to_s
-      raise NoScriptDirectory, "No script_dir set for MongoScript!" unless dir
-      path = File.join(dir, "#{script_name.underscore}.js")
-      raise ScriptNotFound, "Unable to find script #{script_name}" unless File.exists?(path)
-      code = File.read(path)
-      LOADED_SCRIPTS[script_name] ||= code
+    module ClassMethods
+      # code from stored files
+      def code_for(script_name, dir = self.script_dir)
+        script_name = script_name.to_s
+        raise NoScriptDirectory, "No script_dir set for MongoScript!" unless dir
+        path = File.join(dir, "#{script_name.underscore}.js")
+        raise ScriptNotFound, "Unable to find script #{script_name}" unless File.exists?(path)
+        code = File.read(path)
+        LOADED_SCRIPTS[script_name] ||= code
 
-      # for future extension
-      # code_hash = Digest::MD5.hex_digest(code)
-      # LOADED_SCRIPTS[script_name] = {
-      #   :code => code,
-      #   :hash => code_hash,
-      #   :installed => false
-      # }
-      # code
-    end
+        # for future extension
+        # code_hash = Digest::MD5.hex_digest(code)
+        # LOADED_SCRIPTS[script_name] = {
+        #   :code => code,
+        #   :hash => code_hash,
+        #   :installed => false
+        # }
+        # code
+      end
 
-    def self.execute_readonly_routine(script_name, *args)
-      execute_readonly_code(MongoScript.code_for(script_name), *args)
-    end
+      def execute_readonly_routine(script_name, *args)
+        execute_readonly_code(code_for(script_name), *args)
+      end
 
-    def self.execute_readwrite_routine(script_name, *args)
-      execute_readwrite_code(MongoScript.code_for(script_name), *args)
-    end
+      def execute_readwrite_routine(script_name, *args)
+        execute_readwrite_code(code_for(script_name), *args)
+      end
 
-    # raw code
-    # note: to pass in Mongo options for the $exec call, like nolock, you need to call execute directly
-    # since otherwise we have no way to tell Mongo options from an argument list ending in a JS hash
-    def self.execute_readonly_code(code, *args)
-      # for readonly operations, set nolock to true to improve concurrency
-      # http://www.mongodb.org/display/DOCS/Server-side+Code+Execution#Server-sideCodeExecution-NotesonConcurrency
-      execute(code, args, {:nolock => true})
-    end
+      # raw code
+      # note: to pass in Mongo options for the $exec call, like nolock, you need to call execute directly
+      # since otherwise we have no way to tell Mongo options from an argument list ending in a JS hash
+      def execute_readonly_code(code, *args)
+        # for readonly operations, set nolock to true to improve concurrency
+        # http://www.mongodb.org/display/DOCS/Server-side+Code+Execution#Server-sideCodeExecution-NotesonConcurrency
+        execute(code, args, {:nolock => true})
+      end
 
-    def self.execute_readwrite_code(code, *args)
-      execute(code, args, options)
-    end
+      def execute_readwrite_code(code, *args)
+        execute(code, args)
+      end
 
-    private
-
-    def self.execute(code, args = [], options = {})
-      # see http://mrdanadams.com/2011/mongodb-eval-ruby-driver/
-      result = MongoScript.database.command({:$eval => code, args: resolve_arguments(args)}.merge(options))
-      Rails.logger.warn("Mongo server-side JS didn't return okay: 1.0!  Result: #{result.inspect}") unless result["ok"] == 1.0
-      result["retval"]
+      def execute(code, args = [], options = {})
+        # see http://mrdanadams.com/2011/mongodb-eval-ruby-driver/
+        result = MongoScript.database.command({:$eval => code, args: resolve_arguments(args)}.merge(options))
+        unless result["ok"] == 1.0
+          raise ExecutionFailure, "MongoScript.execute JS didn't return {ok: 1.0}!  Result: #{result.inspect}"
+        end
+        result["retval"]
+      end
     end
   end
 end
--- spec/cases/execution_spec.rb
+++ spec/cases/execution_spec.rb
@@ -1,44 +1,53 @@
 require 'spec_helper'
 
 describe MongoScript::Execution do
+  module ObjectWithExecution
+    include MongoScript::ORM::MongoidAdapter
+    include MongoScript::Execution
+  end
+
   before :each do
-    MongoScript::Execution.script_dir = SCRIPTS_PATH
+    ObjectWithExecution.script_dir = SCRIPTS_PATH
   end
 
   it "has a constant for LOADED_SCRIPTS" do
     MongoScript::Execution::LOADED_SCRIPTS.should be_a(Hash)
   end
 
-  it "defines ScriptNotFound error" do
+  it "defines ScriptNotFound error < Errno::ENOENT" do
     MongoScript::Execution::ScriptNotFound.superclass.should == Errno::ENOENT
+  end
+
+  it "defines ExecutionFailure error < RuntimeError" do
+    MongoScript::Execution::ExecutionFailure.superclass.should == RuntimeError
   end
 
   it "has a script_dir accessor" do
     stubby = stub("dir")
-    MongoScript::Execution.script_dir = stubby
-    MongoScript::Execution.script_dir.should == stubby
+    ObjectWithExecution.script_dir = stubby
+    ObjectWithExecution.script_dir.should == stubby
   end
 
-  describe "#code_for" do
+  describe ".code_for" do
     before :all do
       @script_code = File.open(File.join(SCRIPTS_PATH, "sample_script.js")).read
     end
 
     it "loads and returns the code for a given file" do
-      MongoScript::Execution.code_for("sample_script").should == @script_code
+      ObjectWithExecution.code_for("sample_script").should == @script_code
     end
 
     it "loads and returns the code for a given file by symbol" do
-      MongoScript::Execution.code_for(:sample_script).should == @script_code
+      ObjectWithExecution.code_for(:sample_script).should == @script_code
     end
 
     it "stores the value in LOADED_SCRIPTS" do
-      MongoScript::Execution.code_for(:sample_script)
-      MongoScript::Execution::LOADED_SCRIPTS["sample_script"].should == @script_code
+      ObjectWithExecution.code_for(:sample_script)
+      ObjectWithExecution::LOADED_SCRIPTS["sample_script"].should == @script_code
     end
 
     it "raises a ScriptNotFound error if the file doesn't exist" do
-      expect { MongoScript::Execution.code_for("i don't exist") }.to raise_exception(MongoScript::Execution::ScriptNotFound)
+      expect { ObjectWithExecution.code_for("i don't exist") }.to raise_exception(ObjectWithExecution::ScriptNotFound)
     end
 
     it "will look in another directory if provided" do
@@ -47,17 +56,87 @@ describe MongoScript::Execution do
       File.stubs(:exists?).returns(true)
       stubby = stub("script contents")
       File.expects(:read).with(File.join(my_path, "#{my_script}.js")).returns(stubby)
-      MongoScript::Execution.code_for(my_script, my_path).should == stubby
+      ObjectWithExecution.code_for(my_script, my_path).should == stubby
     end
 
     it "raises a NoScriptDirectory error if no directory is provided" do
-      expect { MongoScript::Execution.code_for("sample_script", nil) }.to raise_exception(MongoScript::Execution::NoScriptDirectory)
+      expect { ObjectWithExecution.code_for("sample_script", nil) }.to raise_exception(ObjectWithExecution::NoScriptDirectory)
     end
   end
 
-  pending "sends the eval command through Mongo::Config.database" do
-    code = stub("code")
-    args = stub("args")
-    Mongo::Config.database
+  describe ".execute_readonly_routine" do
+    it "gets and passes the appropriate code and arguments to be run in readonly mode" do
+      args = [1, 2, {}]
+      name = "scriptname"
+      stubby = stub("code")
+      ObjectWithExecution.expects(:code_for).with(name).returns(stubby)
+      ObjectWithExecution.expects(:execute_readonly_code).with(stubby, *args)
+      ObjectWithExecution.execute_readonly_routine(name, *args)
+    end
+  end
+
+  describe ".execute_readwrite_routine" do
+    it "gets and passes the appropriate code and arguments to be run in readwrite mode" do
+      args = [1, 2, {}]
+      name = "scriptname"
+      stubby = stub("code")
+      ObjectWithExecution.expects(:code_for).with(name).returns(stubby)
+      ObjectWithExecution.expects(:execute_readwrite_code).with(stubby, *args)
+      ObjectWithExecution.execute_readwrite_routine(name, *args)
+    end
+  end
+
+  describe ".execute_readonly_code" do
+    it "executes provided code and arguments in with nolock mode" do
+      args = [1, 2, {}]
+      code = stub("code")
+      ObjectWithExecution.expects(:execute).with(code, args, {:nolock => true})
+      ObjectWithExecution.execute_readonly_code(code, *args)
+    end
+  end
+
+  describe ".execute_readwrite_code" do
+    it "executes provided code and arguments with no Mongo options" do
+      args = [1, 2, {}]
+      code = stub("code")
+      ObjectWithExecution.expects(:execute).with(code, args)
+      ObjectWithExecution.execute_readwrite_code(code, *args)
+    end
+  end
+
+  describe ".execute" do
+    it "executes the command via the Mongo database" do
+      MongoScript.database.expects(:command).returns({"ok" => 1.0})
+      ObjectWithExecution.execute("code")
+    end
+
+    it "executes the code using the eval command" do
+      code = stub("code")
+      MongoScript.database.expects(:command).with(has_entries(:$eval => code)).returns({"ok" => 1.0})
+      ObjectWithExecution.execute(code)
+    end
+
+    it "passes in any arguments provided" do
+      args = [:a, :r, :g, :s]
+      MongoScript.database.expects(:command).with(has_entries(:args => args)).returns({"ok" => 1.0})
+      ObjectWithExecution.execute("code", args)
+    end
+
+    it "merges in any additional options" do
+      options = {:a => stub("options")}
+      MongoScript.database.expects(:command).with(has_entries(options)).returns({"ok" => 1.0})
+      ObjectWithExecution.execute("code", [], options)
+    end
+
+    it "raises an ExecutionFailure error if the result[ok] != 1.0" do
+      MongoScript.database.expects(:command).returns({"result" => {}})
+      expect { ObjectWithExecution.execute("code") }.to raise_exception(MongoScript::Execution::ExecutionFailure)
+    end
+
+    it "returns the retval" do
+      result = stub("result")
+      MongoScript.database.expects(:command).returns({"ok" => 1.0, "retval" => result})
+      ObjectWithExecution.execute("code").should == result
+    end
   end
 end
